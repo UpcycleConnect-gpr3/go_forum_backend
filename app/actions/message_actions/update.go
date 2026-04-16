78c684e816a12f811369de5ec1b879f38ee52e4f
Gravityfall <allenpilloygomes@gmail.com>
2026-04-16 21:11:58 +0200

    feat: add message structure and actions

---
+++ app/actions/message_actions/update.go
@@ -0,0 +1,31 @@
+package message_actions
+
+import (
+	"go-forum-backend/app/models/message_models"
+	"go-forum-backend/utils/rules"
+)
+
+type UpdateMessageDTO struct {
+	Content string `json:"content"`
+}
+
+func validateUpdate(dto UpdateMessageDTO) []rules.ValidationError {
+	var errs []rules.ValidationError
+
+	rules.StringMinLength(dto.Content, 1, "content", &errs)
+
+	return errs
+}
+
+func UpdateMessage(id int, dto UpdateMessageDTO) ([]rules.ValidationError, *message_models.Message) {
+	errs := validateUpdate(dto)
+	if len(errs) > 0 {
+		return errs, nil
+	}
+
+	message := message_models.UpdateMessage(id, message_models.UpdateMessageDTO{
+		Content: dto.Content,
+	})
+
+	return nil, message
+}
